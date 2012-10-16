3435cd484841688a9a4dd17f4f3a133f8e1780fb
ayl81 <itsmealda@gmail.com>
2012-10-16 11:43:21 -0700

    Added new view controller for rendering the map.

---
+++ HealthyBytes/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  HealthyBytes
+//
+//  Created by Alda Luong on 10/16/12.
+//  Copyright (c) 2012 Alda Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
