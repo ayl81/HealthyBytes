2456901845e567053a1c028dc7afd8b3b0420da9
ayl81 <itsmealda@gmail.com>
2012-11-01 22:59:52 -0700

    clean up

---
+++ HealthyBytes/HealthRiskAssessmentViewController2.h
@@ -0,0 +1,13 @@
+//
+//  HealthRiskAssessmentViewController2.h
+//  HealthyBytes
+//
+//  Created by Alda Luong on 11/1/12.
+//  Copyright (c) 2012 Alda Luong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HealthRiskAssessmentViewController2 : UITableViewController
+
+@end
